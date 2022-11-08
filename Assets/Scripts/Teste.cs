7df936336b7789f6dbe285660ab14598572c582a
Rogerio Pinheiro <rogerps@tecgraf.puc-rio.br>
2022-11-08 20:44:42 -0300

    Alteração no script de teste

--- Assets/Scripts/Teste.cs
+++ Assets/Scripts/Teste.cs
@@ -7,7 +7,7 @@ public class Teste : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Iniciando script");
     }
 
     // Update is called once per frame
